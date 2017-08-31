396d8940776623985a9461e0618e67ecfc35b25c
harisha Annapareddy <harishaannapareddy@my.unt.edu>
2017-08-31 21:09:12 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,6 +10,6 @@ it('concatenates strings to greet a special guest in `greetSpecialGuest`', funct
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
-it('define `greet`', function() {
+it(function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
